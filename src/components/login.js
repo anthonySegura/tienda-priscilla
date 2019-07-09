703a944a30569d87e6ab54c76d08a3bb12cf00fd
anthonySegura <ansechaves@gmail.com>
2019-07-09 12:31:02 -0600

    interfaz del login y registro

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Buttom from 'react-bootstrap/Button';
+import ReactModalLogin from 'react-modal-login';
+import '../App.css';
 
 class Login extends Component {
 
@@ -11,6 +13,13 @@ class Login extends Component {
       loading: false,
       error: null
     }
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.onLoginFail = this.onLoginFail.bind(this);
+    this.startLoading = this.startLoading.bind(this);
+    this.finishLoading = this.finishLoading.bind(this);
+    this.afterTabsChange = this.afterTabsChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
   openModal() {
@@ -55,11 +64,97 @@ class Login extends Component {
     });
   }
 
+  onLogin() {
+
+  }
+
+  onRegister() {
+
+  }
+
+  logout() {
+
+  }
+
+  handleLogin() {
+    this.state.logged ? this.logout() : this.openModal();
+  }
+
   render() {
     return (
-      <Buttom variant="inline"
-        className={this.state.logged ? "fa fa-sign-out" : "fa fa-sign-in"}>
-      </Buttom>
+      <div style={{ display: "inline-block" }}>
+        <Buttom variant="inline"
+          className={this.state.logged ? "fa fa-sign-out" : "fa fa-sign-in"}
+          onClick={this.handleLogin}>
+        </Buttom>
+        <ReactModalLogin
+          visible={this.state.showModal}
+          onCloseModal={this.closeModal.bind(this)}
+          loading={this.state.loading}
+          error={this.state.error}
+          tabs={{
+            afterChange: this.afterTabsChange.bind(this)
+          }}
+          loginError={{
+            label: "Couldn't sign in, please try again."
+          }}
+          registerError={{
+            label: "Couldn't sign up, please try again."
+          }}
+          startLoading={this.startLoading.bind(this)}
+          finishLoading={this.finishLoading.bind(this)}
+          form={{
+            onLogin: this.onLogin.bind(this),
+            onRegister: this.onRegister.bind(this),
+            loginBtn: {
+              label: "Iniciar Sesión"
+            },
+            registerBtn: {
+              label: "Registrarse"
+            },
+            loginInputs: [
+              {
+                containerClass: 'RML-form-group',
+                label: 'Email',
+                type: 'email',
+                inputClass: 'RML-form-control',
+                id: 'email',
+                name: 'email',
+                placeholder: 'Email',
+              },
+              {
+                containerClass: 'RML-form-group',
+                label: 'Contraseña',
+                type: 'password',
+                inputClass: 'RML-form-control',
+                id: 'password',
+                name: 'password',
+                placeholder: 'contraseña',
+              }
+            ],
+            registerInputs: [
+              {
+                containerClass: 'RML-form-group',
+                label: 'Email',
+                type: 'email',
+                inputClass: 'RML-form-control',
+                id: 'email',
+                name: 'email',
+                placeholder: 'Email',
+              },
+              {
+                containerClass: 'RML-form-group',
+                label: 'Contraseña',
+                type: 'password',
+                inputClass: 'RML-form-control',
+                id: 'password',
+                name: 'password',
+                placeholder: 'contraseña',
+              }
+            ],
+          }}
+        />
+      </div>
     );
   }
 }
